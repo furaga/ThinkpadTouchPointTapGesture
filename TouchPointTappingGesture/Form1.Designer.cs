7977f2e7b2cec20a584c3bca5d5fe1e117e02458
furaga <furaga.fukahori@gmail.com>
2016-03-05 15:03:46 +0900

    first commit

---
+++ TouchPointTappingGesture/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace TouchPointTappingGesture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.labelGesture = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBoxEnableClick = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.checkBoxEnableClick);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(751, 455);
+            this.splitContainer1.SplitterDistance = 197;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.canvas);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.labelGesture);
+            this.splitContainer2.Size = new System.Drawing.Size(550, 455);
+            this.splitContainer2.SplitterDistance = 295;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.Color.White;
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvas.Location = new System.Drawing.Point(0, 0);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(550, 295);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            // 
+            // labelGesture
+            // 
+            this.labelGesture.AutoSize = true;
+            this.labelGesture.Font = new System.Drawing.Font("MS UI Gothic", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelGesture.Location = new System.Drawing.Point(131, 12);
+            this.labelGesture.Name = "labelGesture";
+            this.labelGesture.Size = new System.Drawing.Size(272, 97);
+            this.labelGesture.TabIndex = 0;
+            this.labelGesture.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBoxEnableClick.AutoSize = true;
+            this.checkBoxEnableClick.Location = new System.Drawing.Point(12, 22);
+            this.checkBoxEnableClick.Name = "checkBox1";
+            this.checkBoxEnableClick.Size = new System.Drawing.Size(169, 16);
+            this.checkBoxEnableClick.TabIndex = 0;
+            this.checkBoxEnableClick.Text = "タップ検出時にマウスクリックする";
+            this.checkBoxEnableClick.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 455);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelGesture;
+        private System.Windows.Forms.CheckBox checkBoxEnableClick;
+    }
+}
+
---
+++ TouchPointTappingGesture/Form1.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TouchPointTappingGesture
+{
+    public partial class Form1 : Form
+    {
+        System.Diagnostics.Stopwatch stopWatch = new System.Diagnostics.Stopwatch();
+
+        class TimePoint
+        {
+            public float X { get; private set; }
+            public float Y { get; private set; }
+            public float Time { get; private set; }
+            public TimePoint(float x, float y, float time)
+            {
+                X = x;
+                Y = y;
+                Time = time;
+            }
+        }
+
+        List<TimePoint> mousePosList = new List<TimePoint>();
+        List<TimePoint> mouseVelList = new List<TimePoint>();
+        List<TimePoint> mouseAccList = new List<TimePoint>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        FLib.Hooker hooker = new FLib.Hooker();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            hooker.OnMouseHook += OnMouseHook;
+            hooker.Hook();
+
+            stopWatch.Start();
+        }
+
+        Pen pen0 = new Pen(Brushes.Black);
+        Pen pen1 = new Pen(Brushes.Blue);
+        Pen pen2 = new Pen(Brushes.Red);
+
+        float getScaledIntensity(TimePoint pt)
+        {
+            const float ratioY = 100.0f;
+            
+            float val = (float)Math.Sqrt(pt.X * pt.X + pt.Y * pt.Y);
+            float logVal = (float)Math.Log10(val + 1);
+
+            return logVal * ratioY;
+        }
+
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {
+            float current = stopWatch.ElapsedMilliseconds * 0.001f;
+
+            if (mousePosList.Count >= 2)
+            {
+                e.Graphics.DrawLines(
+                    pen0,
+                    mousePosList.Select(pt =>
+                        new PointF(
+                            e.ClipRectangle.Right - (current - pt.Time) * 100.0f,
+                            getScaledIntensity(pt))
+                    ).ToArray());
+            }
+
+            if (mouseVelList.Count >= 2)
+            {
+                e.Graphics.DrawLines(
+                    pen1,
+                    mouseVelList.Where(pt => pt != null)
+                        .Select(pt =>
+                        new PointF(
+                            e.ClipRectangle.Right - (current - pt.Time) * 100.0f,
+                            getScaledIntensity(pt))
+                    ).ToArray());
+            }
+            if (mouseAccList.Count >= 2)
+            {
+                e.Graphics.DrawLines(
+                    pen2,
+                    mouseVelList.Where(pt => pt != null)
+                        .Select(pt =>
+                        new PointF(
+                            e.ClipRectangle.Right - (current - pt.Time) * 100.0f,
+                            getScaledIntensity(pt))
+                    ).ToArray());
+
+                foreach (var pt in mouseVelList.Where(pt => pt != null)
+                        .Select(pt =>
+                        new PointF(
+                            e.ClipRectangle.Right - (current - pt.Time) * 100.0f,
+                            getScaledIntensity(pt))
+                    ))
+                {
+                    e.Graphics.FillRectangle(Brushes.Red, new RectangleF(pt.X - 3, pt.Y - 3, 6, 6));
+                }
+            }
+
+            foreach (var time in detecttimeList)
+            {
+                float detectX = e.ClipRectangle.Right - (current - time) * 100.0f;
+                e.Graphics.DrawLine(pen1, new PointF(detectX, 0), new PointF(detectX, e.ClipRectangle.Height));
+            }
+
+
+        }
+
+        TimePoint getCurrentPointInfo()
+        {
+            float time = stopWatch.ElapsedMilliseconds * 0.001f;
+
+            if (mousePosList.Count >= 1)
+            {
+                float lastTime = mousePosList[mousePosList.Count - 1].Time;
+                if (time - lastTime <= 1e-2f)
+                {
+                    return null;
+                }
+            }
+
+            Point curPt = Cursor.Position;
+            return new TimePoint(curPt.X, curPt.Y, time);
+        }
+
+        void addMousePointInfo(TimePoint pt)
+        {
+            if (pt == null)
+            {
+                return;
+            }
+
+            float time = pt.Time;
+
+            mousePosList.Add(pt);
+
+            if (mousePosList.Count >= 2)
+            {
+                float dx = mousePosList[mousePosList.Count - 1].X - mousePosList[mousePosList.Count - 2].X;
+                float dy = mousePosList[mousePosList.Count - 1].Y - mousePosList[mousePosList.Count - 2].Y;
+                float dt = (mousePosList[mousePosList.Count - 1].Time - mousePosList[mousePosList.Count - 2].Time);
+                mouseVelList.Add(new TimePoint(dx / dt, dy / dt, time));
+            }
+
+            if (mouseVelList.Count >= 2)
+            {
+                float dx = mouseVelList[mouseVelList.Count - 1].X - mouseVelList[mouseVelList.Count - 2].X;
+                float dy = mouseVelList[mouseVelList.Count - 1].Y - mouseVelList[mouseVelList.Count - 2].Y;
+                float dt = (mouseVelList[mouseVelList.Count - 1].Time - mouseVelList[mouseVelList.Count - 2].Time);
+                mouseAccList.Add(new TimePoint(dx / dt, dy / dt, time));
+            }
+
+            while (mouseAccList.Count > 1000)
+            {
+                mousePosList.RemoveAt(0);
+                mouseVelList.RemoveAt(0);
+                mouseAccList.RemoveAt(0);
+            }
+        }
+
+        bool OnMouseHook(int code, FLib.WM message, IntPtr state, FLib.Hooker hooker)
+        {
+            switch (message)
+            {
+                case FLib.WM.MOUSEMOVE:
+                    float time = stopWatch.ElapsedMilliseconds * 0.001f;
+
+                    if (mousePosList.Count >= 1)
+                    {
+                        float lastTime = mousePosList[mousePosList.Count - 1].Time;
+                        if (time - lastTime <= 1e-2f)
+                        {
+                            break;
+                        }
+                    }
+
+                    addMousePointInfo(getCurrentPointInfo());
+                    
+                    if (checkGesture_dirty())
+                    {
+                        click();
+                    }
+
+                    break;
+            }
+
+            return false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            addMousePointInfo(getCurrentPointInfo());
+
+            if (checkGesture_dirty())
+            {
+                click();
+            }
+
+            canvas.Invalidate();
+        }
+
+        List<float> detecttimeList = new List<float>();
+        float lastDetectTime = 0;
+
+        private bool checkGesture_dirty()
+        {
+            if (mouseAccList.Count < 16)
+            {
+                labelGesture.Text = "";
+                return false;
+            }
+
+            float time = mouseAccList[mouseAccList.Count - 1].Time;
+
+            if (time - lastDetectTime < 0.3f)
+            {
+                labelGesture.Text = "";
+                return false;
+            }
+
+
+            labelGesture.Text = "";
+
+            const int lookFrameOffset0 = -9;
+            const int lookFrameOffset1 = -6;
+            const int lookFrameOffset2 = -2;
+
+            for (int i = mouseAccList.Count + lookFrameOffset0; i < mouseAccList.Count + lookFrameOffset1; i++)
+            {
+                // 全部小さい
+                if (getScaledIntensity(mouseAccList[i]) < 100)
+                {
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            float newDetecttTime = time;
+            bool anyHighIntensity = false;
+            for (int i = mouseAccList.Count + lookFrameOffset1; i < mouseAccList.Count + lookFrameOffset2; i++)
+            {
+                // 最低ひとつが大きい
+                if (getScaledIntensity(mouseAccList[i]) >= 30)
+                {
+                    anyHighIntensity = true;
+                    newDetecttTime = mouseAccList[i].Time;
+                }
+            }
+
+            if (false == anyHighIntensity)
+            {
+                return false;
+            }
+
+            for (int i = mouseAccList.Count + lookFrameOffset2; i < mouseAccList.Count; i++)
+            {
+                // 全部小さい
+                if (getScaledIntensity(mouseAccList[i]) < 100)
+                {
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            labelGesture.Text = "Tapped!";
+
+            lastDetectTime = newDetecttTime;
+            
+            detecttimeList.Add(newDetecttTime);
+            while (detecttimeList.Count > 100)
+            {
+                detecttimeList.RemoveAt(0);
+            }
+
+            return true;
+        }
+
+
+        void click()
+        {
+            if (checkBoxEnableClick.Checked)
+            {
+                int x = Cursor.Position.X;
+                int y = Cursor.Position.Y;
+                mouse_event((UInt32)MouseEventFlags.LEFTDOWN, (uint)x, (uint)y, 0, IntPtr.Zero);
+                mouse_event((UInt32)MouseEventFlags.LEFTUP, (uint)x, (uint)y, 0, IntPtr.Zero);
+            }
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern void mouse_event(UInt32 dwFlags, UInt32 dx, UInt32 dy, UInt32 dwData, IntPtr dwExtraInfo);
+        public enum MouseEventFlags : uint
+        {
+            LEFTDOWN = 0x00000002,
+            LEFTUP = 0x00000004,
+            MIDDLEDOWN = 0x00000020,
+            MIDDLEUP = 0x00000040,
+            MOVE = 0x00000001,
+            ABSOLUTE = 0x00008000,
+            RIGHTDOWN = 0x00000008,
+            RIGHTUP = 0x00000010,
+            WHEEL = 0x00000800,
+            XDOWN = 0x00000080,
+            XUP = 0x00000100
+        }
+    }
+}
